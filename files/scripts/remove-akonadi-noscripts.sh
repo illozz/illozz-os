a6d7de71d792493b83bfd2511e7db45c13926ee2
illozz <github@zhst.eu>
2026-08-08 18:55:49 +0200

    akonadi change

--- files/scripts/remove-akonadi-noscripts.sh
+++ files/scripts/remove-akonadi-noscripts.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-set -eo pipefail
-rpm -e --nodeps --noscripts akonadi-server akonadi-server-mysql kf5-akonadi-server kf5-akonadi-server-mysql qt6-qtbase-mysql 2>/dev/null || true
+set -euxo pipefail
+
+for package in akonadi-server-mysql qt6-qtbase-mysql; do
+  if rpm -q "$package" >/dev/null 2>&1; then
+    rpm -e --nodeps --noscripts "$package"
+  fi
+done
